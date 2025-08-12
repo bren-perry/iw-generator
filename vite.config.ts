47fb20cd1474e75171c61e790ce47f423e79f49b
bren-perry <brennen.perry.tgps@gmail.com>
2025-08-12 14:36:49 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// IMPORTANT: use your repo name here
 export default defineConfig({
+  base: '/iw-generator/',
   plugins: [react()],
 })
